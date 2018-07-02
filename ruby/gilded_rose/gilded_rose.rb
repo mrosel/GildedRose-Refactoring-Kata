b47d708f025fba8d6c28631992d844fc607152d8
mac <mac@bbtnetworks.com>
2018-07-02 12:18:49 +0100

    new class structure

--- ruby/gilded_rose/gilded_rose.rb
+++ ruby/gilded_rose/gilded_rose.rb
@@ -1,7 +1,18 @@
+require 'active_support/concern'
 require_relative 'item'
 
 class GildedRose
   attr_reader :items
+
+  # TODO: These really belong to item and only the business logic here
+  # comparison of strings == 'bad'
+  SPECIAL_ITEMS_PROPERTY = {
+    "Sulfuras, Hand of Ragnaros" => :legendary,
+    "Conjured Mana Cake" => :conjured,
+    "Aged Brie" => :collectable, # prices increase with time
+    "Backstage passes to a TAFKAL80ETC concert" => :limited_event # prices increase with near event, worthless after
+  }
+
 
   def initialize(items)
     @items = items
@@ -9,6 +20,15 @@ class GildedRose
 
   def update_quality()
     @items.each do |item|
+      type = (SPECIAL_ITEMS_PROPERTY[item.name] || 'Normal').to_s.capitalize
+      classname = type.to_s.split('_').map{|s| s.capitalize}.join
+      Module.const_get("#{classname}Item").change(item)
+    end
+  end
+end
+
+class NormalItem
+  def self.change(item)
       if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert"
         if item.quality > 0
           if item.name != "Sulfuras, Hand of Ragnaros"
@@ -53,5 +73,26 @@ class GildedRose
         end
       end
     end
-  end
+end
+
+class LimitedEventItem < NormalItem
+end
+
+class LegendaryItem < NormalItem
+  # def self.change(item)
+
+  # end
+end
+
+
+class ConjuredItem < NormalItem
+  # def self.change(item)
+
+  # end
+end
+
+class CollectableItem < NormalItem
+  # def self.change(item)
+
+  # end
 end
