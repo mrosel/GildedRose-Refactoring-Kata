0afd17f92a14ec02ab51b6792d1f8f71dbf2031a
mac <mac@bbtnetworks.com>
2018-07-02 12:47:42 +0100

    red/green pass for normal items

--- ruby/gilded_rose/gilded_rose.rb
+++ ruby/gilded_rose/gilded_rose.rb
@@ -29,70 +29,36 @@ end
 
 class NormalItem
   def self.change(item)
-      if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert"
-        if item.quality > 0
-          if item.name != "Sulfuras, Hand of Ragnaros"
-            item.quality = item.quality - 1
-          end
-        end
-      else
-        if item.quality < 50
-          item.quality = item.quality + 1
-          if item.name == "Backstage passes to a TAFKAL80ETC concert"
-            if item.sell_in < 11
-              if item.quality < 50
-                item.quality = item.quality + 1
-              end
-            end
-            if item.sell_in < 6
-              if item.quality < 50
-                item.quality = item.quality + 1
-              end
-            end
-          end
-        end
-      end
-      if item.name != "Sulfuras, Hand of Ragnaros"
-        item.sell_in = item.sell_in - 1
-      end
-      if item.sell_in < 0
-        if item.name != "Aged Brie"
-          if item.name != "Backstage passes to a TAFKAL80ETC concert"
-            if item.quality > 0
-              if item.name != "Sulfuras, Hand of Ragnaros"
-                item.quality = item.quality - 1
-              end
-            end
-          else
-            item.quality = item.quality - item.quality
-          end
-        else
-          if item.quality < 50
-            item.quality = item.quality + 1
-          end
-        end
-      end
+    @item = item
+    sell_in!
+    quality!
+  end
+
+  def self.sell_in!
+    @item.sell_in = @item.sell_in - 1
+  end
+
+  def self.quality!
+    @item.quality = @item.quality - 1 unless @item.quality == 0
+    # additional depreciation for sell in
+    if @item.quality >= 1 && @item.sell_in < 0
+      @item.quality = @item.quality - 1
     end
+  end
 end
 
+# TODO: this makes more sense as a mixin rather than inheritence
 class LimitedEventItem < NormalItem
 end
 
 class LegendaryItem < NormalItem
-  # def self.change(item)
-
-  # end
+  def change(item)
+  end
 end
 
-
 class ConjuredItem < NormalItem
-  # def self.change(item)
-
-  # end
 end
 
 class CollectableItem < NormalItem
-  # def self.change(item)
 
-  # end
 end
